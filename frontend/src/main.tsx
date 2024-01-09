32756dde3ffdbd119b2dcb11a28fdf130b053bbc
DenZaiyy <denzaiyy@outlook.fr>
2024-01-09 14:36:54 +0100

    feat: add notistack package to have a beautiful notification

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './assets/styles/main.css'
 import {BrowserRouter} from "react-router-dom"
+import {SnackbarProvider} from "notistack"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+	<BrowserRouter>
+		<SnackbarProvider>
+			<App/>
+		</SnackbarProvider>
+	</BrowserRouter>,
 )
